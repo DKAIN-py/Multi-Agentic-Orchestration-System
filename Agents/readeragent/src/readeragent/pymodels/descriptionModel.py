e9045229b5f2839f8c5d54e1a383b87ffb760bc0
DKAIN-py <divyanshu06kain@gmail.com>
2026-02-05 22:11:57 +0530

    Cleanup

--- Agents/readeragent/src/readeragent/pymodels/descriptionModel.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel, Field
-
-class DescriptionExtracter(BaseModel):
-    description: str = Field(...,description="Information to be retrived from the given document by user")
-    filename: str = Field(...,description="name of the file to be processed")
--- Agents/writeragent/src/writeragent/pymodels/ContentExtracter.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel, Field
-
-class ContentExtracter(BaseModel):
-    content: str = Field(...,description="Content to be formatted and then writen in the file")
-    filename: str = Field(..., description="Dummy name for the file to be created along with appropriate file extension")
